3349b99e4fb9c62e9d2ad5f10c5a5516fe8ed88a
Fumiya Sakai <just1factory@gmail.com>
2019-07-23 16:01:22 +0900

    Initial Commit

---
+++ InfiniteScrollAnotherPattern/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InfiniteScrollAnotherPattern
+//
+//  Created by 酒井文也 on 2019/07/23.
+//  Copyright © 2019 酒井文也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
