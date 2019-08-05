1cdd340dff165a0c1ccc0e60de0d39170a7719ab
Fumiya Sakai <just1factory@gmail.com>
2019-08-05 15:10:25 +0900

    各種表示View要素に関する定義を追加

---
+++ InfiniteScrollAnotherPattern/View/CarouselContentsView/CarouselContentsView.swift
@@ -0,0 +1,40 @@
+//
+//  CarouselContentsView.swift
+//  InfiniteScrollAnotherPattern
+//
+//  Created by 酒井文也 on 2019/08/03.
+//  Copyright © 2019 酒井文也. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+final class CarouselContentsView: CustomViewBase {
+
+    var carouselTitle: String?
+    var contentKey: String?
+
+    @IBOutlet private weak var carouselTitleLabel: UILabel!
+
+    // MARK: - Static Function
+
+    // MEMO: 他のView要素からこのView要素を表示・生成するためのメソッド
+    class func make() -> CarouselContentsView {
+        let nib = UINib(nibName: "CarouselContentsView", bundle: nil)
+        let view = nib.instantiate(withOwner: self, options: nil).first as! CarouselContentsView
+        return view
+    }
+
+    // MARK: - Function
+
+    // コンテンツ表示用タイトルをセットする
+    func setContents(title: String, contentKey: String) {
+
+        // コンテンツ表示用のキー値を取得する
+        self.contentKey = contentKey
+
+        // コンテンツ文字列をラベルにセットする
+        carouselTitle = title
+        carouselTitleLabel.text = title
+    }
+}
