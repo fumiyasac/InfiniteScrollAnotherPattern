053e6e63b16a46eefe3889e89c0f0f4a86c9934b
Fumiya Sakai <just1factory@gmail.com>
2019-08-11 22:17:34 +0900

    [WIP] 画面要素の組み立て開始

--- InfiniteScrollAnotherPattern/ViewController/MainViewController.swift
+++ InfiniteScrollAnotherPattern/ViewController/MainViewController.swift
@@ -8,9 +8,52 @@
 
 import UIKit
 
+// MEMO: 元記事＆元リポジトリを参考に無限すスクロールを実装する
+// ← 元記事:
+// ← 元リポジトリ:
+
 final class MainViewController: UIViewController {
+
+    @IBOutlet weak private var carouselScrollView: CarouselScrollView!
+    @IBOutlet weak private var selectionCarouselScrollView: CarouselScrollView!
+    @IBOutlet weak private var paginateScrollView: UIScrollView!
+
+    private var carouselContentsViews: [CarouselContentsView] = []
+
+    // MARK: - Enum
+
+    private enum displayPosition: Int {
+        // MEMO: コンテンツ表示部分のスクロール位置のEnum定義
+        case left
+        case center
+        case right
+
+        // MEMO: スクロール時に表示内容を入れ替えるために利用する
+        func inverse() -> displayPosition {
+            switch self {
+            case .left:
+                return .right
+            case .center:
+                return .center
+            case .right:
+                return .left
+            }
+        }
+    }
+
+    // MARK: - Computed Property
+
+    var selectedCarouselContentsView: CarouselContentsView? {
+        return nil
+    }
+
+    // MARK: - Override
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+}
+
+
+extension MainViewController: UIScrollViewDelegate {
 }
