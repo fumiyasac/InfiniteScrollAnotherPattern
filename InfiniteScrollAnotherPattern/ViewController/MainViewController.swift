70838c148aa906165d1dc49988a05438cbf4c587
Fumiya Sakai <just1factory@gmail.com>
2021-01-06 20:12:32 +0900

    [WIP] 無限スクロールタブ用の土台を作成

--- InfiniteScrollAnotherPattern/ViewController/MainViewController.swift
+++ InfiniteScrollAnotherPattern/ViewController/MainViewController.swift
@@ -19,8 +19,42 @@ import UIKit
 final class MainViewController: UIViewController {
 
     // MEMO: InterfaceBuilder上における設定のポイント
+    // 1つのStoryboardの中で下記3つの部品を配置する
+
+    // (1) categoryTabCollectionView:
+    // 無限スクロールをするためのタブ型表現用UICollectionView
+
+    // (2) categoryTabSelectBarView:
+    // 無限スクロールタブ表示をした文字表示と連動するバー表示をするUIView
+
+    // (3) categoryContentsContainerView:
+    // 無限スクロールタブ表示をしたカテゴリーに紐づくコンテンツ一覧表示をするContainerView
 
     // MARK: - Properties
+
+    // 現在表示しているViewControllerのタグ番号
+    private var selectedPageViewControllerIndex: Int = 0
+
+    // ページングして表示させるViewControllerを保持する配列
+    private var targetViewControllerLists: [UIViewController] = []
+
+    // ContainerViewにEmbedしたUIPageViewControllerのインスタンスを保持する
+    private var pageViewController: UIPageViewController? = nil
+
+    // 現在のセル番号
+    private var selectedCollectionViewIndex: Int!
+
+    // タブ型UICollectionViewにおけるX軸方向のScroll開始位置
+    private var startCollectionViewPosX: CGFloat = 0
+
+    // タブ型UICollectionViewにおける表示セルをコピーする倍数
+    private let cellCopyCount: Int = 5
+
+    // MARK: - @IBOutlet
+
+    @IBOutlet private weak var categoryTabCollectionView: UICollectionView!
+    @IBOutlet private weak var categoryTabSelectBarView: UIView!
+    @IBOutlet private weak var categoryContentsContainerView: UIView!
 
     // MARK: - Computed Property
 
@@ -28,7 +62,23 @@ final class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        setupCategoryTabCollectionView()
+        setupCategoryTabSelectBarView()
+        setupCategoryContentsContainerView()
     }
 
     // MARK: - Private Function
+
+    private func setupCategoryTabCollectionView() {
+        
+    }
+
+    private func setupCategoryTabSelectBarView() {
+        
+    }
+
+    private func setupCategoryContentsContainerView() {
+        
+    }
 }
