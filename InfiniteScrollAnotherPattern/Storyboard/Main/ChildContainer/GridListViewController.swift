d8294e2bb2b7630fb59ab15827f420146bd7f12b
Fumiya Sakai <just1factory@gmail.com>
2021-02-21 23:13:51 +0900

    カテゴリーに対応する想定の画面ベースを新規作成

---
+++ InfiniteScrollAnotherPattern/Storyboard/Main/ChildContainer/GridListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  GridListViewController.swift
+//  InfiniteScrollAnotherPattern
+//
+//  Created by 酒井文也 on 2021/02/21.
+//  Copyright © 2021 酒井文也. All rights reserved.
+//
+
+import UIKit
+
+final class GridListViewController: UIViewController {
+
+    // MARK: - Override
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
