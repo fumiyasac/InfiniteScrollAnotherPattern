25834fb54b744afed2d74237df1f0ec2f8b5457e
Fumiya Sakai <just1factory@gmail.com>
2021-03-20 21:08:01 +0900

    WIP 読み込み失敗時のViewクラスを新規追加

---
+++ InfiniteScrollAnotherPattern/Interfaces/View/TabContentsErrorView/TabContentsErrorView.swift
@@ -0,0 +1,23 @@
+//
+//  TabContentsErrorView.swift
+//  InfiniteScrollAnotherPattern
+//
+//  Created by 酒井文也 on 2021/03/20.
+//  Copyright © 2021 酒井文也. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// MARK: - TabContentsErrorViewDelegate
+
+protocol TabContentsErrorViewDelegate: NSObjectProtocol {
+    func reload()
+}
+
+class TabContentsErrorView: CustomViewBase {
+
+    // MARK: - Property
+
+    weak var delegate: TabContentsErrorViewDelegate?
+}
