f7e4601e9b64219ecde98a50af69806a2c6a8342
Fumiya Sakai <just1factory@gmail.com>
2021-03-28 01:15:11 +0900

    WIP カラーパレット定義

--- InfiniteScrollAnotherPattern/Constants/AppConstants.swift
+++ InfiniteScrollAnotherPattern/Constants/AppConstants.swift
@@ -20,6 +20,12 @@ struct Constants {
     struct Colors {
         static let primary: UIColor = UIColor(code: "#ffa000")
         static let secondary: UIColor = UIColor(code: "#ffc107")
+        static let tertiary: UIColor = UIColor(code: "#ffe082")
+        static let accentPrimary: UIColor = UIColor(code: "#689f38")
+        static let accentSecondary: UIColor = UIColor(code: "#c5e1a5")
+        static let letterPrimary: UIColor = UIColor(code: "#333333")
+        static let letterSecondary: UIColor = UIColor(code: "#aaaaaa")
+        static let letterTertiary: UIColor = UIColor(code: "#ccccc")
         static let navigationBarTitle: UIColor = UIColor.white
         static let categoryScrollTabDefault: UIColor = UIColor(code: "#bbbbbb")
         static let categoryScrollTabActive: UIColor = UIColor(code: "#efefef")
