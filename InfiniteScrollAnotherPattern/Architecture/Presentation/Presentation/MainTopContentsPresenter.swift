632fcd5c90837d7453cdadada59e85a0d1b78468
Fumiya Sakai <just1factory@gmail.com>
2021-04-11 21:15:32 +0900

    WIP MVVM→MVPへ設計を戻す

--- InfiniteScrollAnotherPattern/Architecture/Presentation/Presentation/MainTopContentsPresenter.swift
+++ InfiniteScrollAnotherPattern/Architecture/Presentation/Presentation/MainTopContentsPresenter.swift
@@ -1,8 +1,8 @@
 //
-//  TopContensViewModel.swift
+//  MainTopContentsPresenter.swift
 //  InfiniteScrollAnotherPattern
 //
-//  Created by 酒井文也 on 2021/04/05.
+//  Created by 酒井文也 on 2021/04/11.
 //  Copyright © 2021 酒井文也. All rights reserved.
 //
 
